75ca8aac88cfb02c1683cd2bb535d0dd0bfdf964
ASP1527 <66624024+ASP1527@users.noreply.github.com>
2020-06-30 16:52:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var number = 0;
+
+function addNumber() {
+  number += 1;
+  document.getElementById("number").innerHTML = number;
+}
+
+function subtractNumber() {
+  number -= 1;
+  document.getElementById("number").innerHTML = number;
+}
